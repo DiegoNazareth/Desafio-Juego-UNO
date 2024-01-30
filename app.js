9937167ffd089aa40a69007ccc1e2c9c5c8d2fb0
DiegoNazareth <toby72korn@hotmail.com>
2024-01-30 00:03:17 -0600

    Subiendo juego a git

--- app.js
+++ app.js
@@ -1,9 +1,40 @@
-let numeroSecreto=6;
-let numeroUsuario=prompt("Me indicas un numero porfavor: ");
-//alert('Hola Mundo');
+//variables
+let numeroMaximoPosible=100
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible+1);
+let numeroUsuario = 0;
+let intento=1;
+//let palabraVeces='vez';
+let maximosIntentos=6;
 
-console.log(numeroUsuario);
+while (numeroSecreto != numeroUsuario) {
+  let numeroUsuario = parseInt( prompt(`Me indicas un numero entre 1 y ${numeroMaximoPosible} porfavor:` ));
+  //alert('Hola Mundo');
 
-if(numeroSecreto==numeroUsuario){
-    alert('Acertarste el numero');
+  console.log(typeof(numeroUsuario));
+
+  /*
+Este codigo realiza la comparacion 
+*/
+  if (numeroSecreto == numeroUsuario) {
+    //Acertamos fue verdadera la condicion
+    alert(`Acertarste el numero, el numero es: ${numeroSecreto}. Los hiciste en ${intento} ${intento==1 ? 'vez' : 'veces'}`);
+    break;
+  } else {
+    if (numeroUsuario > numeroSecreto) {
+      alert("El numero secreto es menor");
+    } else {
+      alert("El numero secreto es mayor");
+    }
+    if(intento>=maximosIntentos){
+      alert(`Llegaste al numero maximo de ${maximosIntentos} intentos`);
+      break;
+    }
+    //Incrementamos el contador cuando no acierta
+    //intento=intento+1;
+    intento++;
+    
+    //palabraVeces='veces';
+    //La condicion no se cumplio
+    alert("Lo siento no acertarste el numero");
+  }
 }
